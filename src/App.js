b8885b71229fea4586c143057d0ced2fd4170af6
Fayaz <fayazazeez2@gmail.com>
2023-07-18 18:52:29 +0530

    home section completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css';
+import Header from './components/header/Header';
+import Home from './pages/home/home';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Router>
+        <Header/>
+        <Routes>
+          <Route index element={<Home/>}></Route>
+          <Route path='movie/:id' element={<h1>movie detail page</h1>}></Route>
+          <Route path='movies/:type' element={<h1>Movies list page</h1>}></Route>
+          <Route path='/*' element={<h1>Error page</h1>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
