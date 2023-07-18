13ec846b8dc2f8d123b2d8107db4d202d6bd1637
Fayaz <fayazazeez2@gmail.com>
2023-07-18 20:13:22 +0530

    cards added and details displayed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css';
 import Header from './components/header/Header';
 import Home from './pages/home/home';
+import MovieList from './components/movieList/movieList';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Routes>
           <Route index element={<Home/>}></Route>
           <Route path='movie/:id' element={<h1>movie detail page</h1>}></Route>
-          <Route path='movies/:type' element={<h1>Movies list page</h1>}></Route>
+          <Route path='movies/:type' element={<MovieList />}></Route>
           <Route path='/*' element={<h1>Error page</h1>}></Route>
         </Routes>
       </Router>
