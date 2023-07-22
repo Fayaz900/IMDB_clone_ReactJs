cdf3c9398581dd33b4380d5f8c8c41494bafd4f7
Fayaz <fayazazeez2@gmail.com>
2023-07-22 16:18:53 +0530

    project done

--- src/components/header/card/card.jsx
+++ src/components/header/card/card.jsx
@@ -23,7 +23,7 @@ function Card({movie}) {
                 </SkeletonTheme>
             </div>
             :
-            <Link to={`movie/${movie.id}`} style={{textDecoration:"none", color:"white"}}>
+            <Link to={`/movie/${movie.id}`} style={{textDecoration:"none", color:"white"}}>
                 <div className="cards">
                     <img src={`https://image.tmdb.org/t/p/original${movie ? movie.poster_path:""}`} alt="" className="cards__img" />
                     <div className="cards__overlay">
